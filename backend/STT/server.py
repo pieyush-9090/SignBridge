d6d7808665b7f27397770111ce31fc14abf8513d
Ayushman Rout <132610289+Ayushmanonlycode@users.noreply.github.com>
2025-07-06 19:04:31 +0530

    Update server.py

--- backend/STT/server.py
+++ backend/STT/server.py
@@ -57,4 +57,4 @@ def predict():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)  # Allows access from different devices
+    app.run(host="0.0.0.0", port=port)  # Allows access from different devices
