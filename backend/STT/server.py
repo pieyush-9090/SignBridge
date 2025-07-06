2ac29e8070c846fda4ca553ff763b4a8f30231e2
Ayushmanonlycode <ayushman.onlycode69@gmail.com>
2025-07-06 18:39:13 +0530

    deployment backend!

--- backend/STT/server.py
+++ backend/STT/server.py
@@ -56,4 +56,5 @@ def predict():
     return jsonify({"error": "Invalid file format"}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")  # Allows access from different devices
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)  # Allows access from different devices
